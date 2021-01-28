70b84311a7c94a043bb16ef1f9533e332a00f5da
SL-m <69431730+SL-m@users.noreply.github.com>
2021-01-28 20:19:25 +0100

    album and track search working with error warning now implemented

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 //let userName = "";
 
+console.log();
+let totalTime = 0;
+let error = false;
+
 
 function s2h(){
         let userName = document.getElementById("userNameInput").value;
+        userName.replace(" ", "+");
         let artist = document.getElementById("artistInput").value;
+        artist.replace(" ", "+");
         let name = document.getElementById("taInput").value;
+        name.replace(" ", "+");
             
         if (userName != "" && artist != "" && userName != "" ){
+            $("#result").empty(); 
+            $('#result').append("<hr>");
             if(getSelectedValue("queryOption") == "track"){
                 getTrackLT(userName, artist, name);
             }
@@ -21,47 +30,64 @@ function s2h(){
          alert("Please fill all thingies");   
         }
 }
+
+function throwError(json){
+    
+        $('#error').append("<h1> ERROR : " + json.message + " </h1>");
+        error = true;
+}
         
         
 function getAlbumLT(user, artist, album){
+    totalTime=0;
     let tracks = [];
-    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=album.getInfo&user="+ user + "&api_key=7f18ca9d34c83965fff9d9ff7f81a740&limit=10&artist=" + artist + "&track=" + album + "&format=json&autocorrect=1", function(json) {
-            var html = '';
+    var totalTimeHtml = '';
+    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=album.getInfo&user="+ user + "&api_key=7f18ca9d34c83965fff9d9ff7f81a740&limit=10&artist=" + artist + "&album=" + album + "&format=json&autocorrect=1", function(json) {
+        if(json.error != undefined){
+            throwError(json);
+            return;
+        }
             $.each(json, function(i, item) {
                 let jsonTracks = item.tracks.track;
-                for(i=0; i<tracks.length; i++){
-                    tracks.add(jsonTracks[i].name);
+                for(i=0; i<jsonTracks.length; i++){
+                    tracks.push(jsonTracks[jsonTracks.length - 1 - i].name);
                 }
             });
-            let totalTime = 0;
-            for(i=0; i<tracks.length; i++){
-                totalTime += getTrackLT(user, artist, tracks[i]);
+            for(i=0; i<jsonTracks.length; i++){
+                getTrackLT(user, artist, tracks[i]);
             }
-            html+= "<h3> Total listened time : " + totalTime + "</h2>";
-            $('#result').append(html);
         });
-    
+        $(document).ajaxStop(function () {
+            if(!error){
+                totalTimeHtml = "<h3> You listened to " + album.replace("+", " ") + " a total of " + totalTime + " minutes or " +totalTime/60 + " hours ! </h3>";
+                $('#result').append(totalTimeHtml);
+            }
+    });
 }
         
         
 function getTrackLT(user, artist, track){
-        
         $.getJSON("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&user="+ user + "&api_key=7f18ca9d34c83965fff9d9ff7f81a740&limit=10&artist=" + artist + "&track=" + track + "&format=json&autocorrect=1", function(json) {
+            if(json.error != undefined){
+            throwError(json);
+            return;
+            }
             var html = '';
             $.each(json, function(i, item) {
-                var timeListened = getPlayTime(item);
+                timeListened = getPlayTime(item);
                 html += constructResult(item, timeListened);
             });
             $('#result').append(html);
         });
-        
-        return timeListened;
+    
 }
+
 
 function getPlayTime(trackJson){
             let name = trackJson.name;
             let duration = trackJson.duration; 
             let playcount = trackJson.userplaycount;
+            totalTime+= ( parseInt( (duration*playcount)/60000, 10) );
             return ( parseInt( (duration*playcount)/60000, 10) );
     
 }
