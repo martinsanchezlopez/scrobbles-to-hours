2f93293e4a62356e856d9830f1308c646bdba2f3
Martin Sanchez Lopez <ireviive@gmail.com>
2021-02-04 20:04:32 +0100

    table style

---
+++ test.js
@@ -0,0 +1,122 @@
+let testArray = [
+  {
+    "rank": "1",
+    "artist": "Defeated Sanity",
+    "name": "Into The Soil",
+    "playcount": 28,
+    "playcountMinute": 115,
+    "playcountHour": "1.9"
+  },
+  {
+    "rank": "2",
+    "artist": "Horrendous",
+    "name": "Monarch",
+    "playcount": 28,
+    "playcountMinute": 113,
+    "playcountHour": "1.9"
+  },
+  {
+    "rank": "3",
+    "artist": "Basalte",
+    "name": "Hémorragie céleste",
+    "playcount": 27,
+    "playcountMinute": 0,
+    "playcountHour": "0.0"
+  },
+  {
+    "rank": "4",
+    "artist": "Defeated Sanity",
+    "name": "Consuming Grief",
+    "playcount": 27,
+    "playcountMinute": 90,
+    "playcountHour": "1.5"
+  },
+  {
+    "rank": "5",
+    "artist": "Defeated Sanity",
+    "name": "Insecta Incendium",
+    "playcount": 27,
+    "playcountMinute": 70,
+    "playcountHour": "1.2"
+  },
+  {
+    "rank": "6",
+    "artist": "Defeated Sanity",
+    "name": "Phytodigestion",
+    "playcount": 27,
+    "playcountMinute": 74,
+    "playcountHour": "1.2"
+  },
+  {
+    "rank": "7",
+    "artist": "Defeated Sanity",
+    "name": "Imposed Corporeal Inhabitation",
+    "playcount": 26,
+    "playcountMinute": 62,
+    "playcountHour": "1.0"
+  },
+  {
+    "rank": "8",
+    "artist": "Defeated Sanity",
+    "name": "Arboreously Transfixed",
+    "playcount": 25,
+    "playcountMinute": 65,
+    "playcountHour": "1.1"
+  },
+  {
+    "rank": "9",
+    "artist": "Defeated Sanity",
+    "name": "Conceived Through Savagery",
+    "playcount": 25,
+    "playcountMinute": 82,
+    "playcountHour": "1.4"
+  },
+  {
+    "rank": "10",
+    "artist": "Defeated Sanity",
+    "name": "Propelled Into Sacrilege",
+    "playcount": 25,
+    "playcountMinute": 108,
+    "playcountHour": "1.8"
+  },
+  {
+    "rank": "11",
+    "artist": "Wayfarer",
+    "name": "The Crimson Rider (Gallows Frontier, Act I)",
+    "playcount": 25,
+    "playcountMinute": 266,
+    "playcountHour": "4.4"
+  },
+  {
+    "rank": "12",
+    "artist": "Defeated Sanity",
+    "name": "Drivelling Putrefaction",
+    "playcount": 24,
+    "playcountMinute": 146,
+    "playcountHour": "2.4"
+  },
+  {
+    "rank": "13",
+    "artist": "Defeated Sanity",
+    "name": "Generosity Of The Deceased",
+    "playcount": 24,
+    "playcountMinute": 92,
+    "playcountHour": "1.5"
+  },
+  {
+    "rank": "14",
+    "artist": "Defeated Sanity",
+    "name": "Suttee",
+    "playcount": 24,
+    "playcountMinute": 69,
+    "playcountHour": "1.2"
+  },
+  {
+    "rank": "15",
+    "artist": "Defeated Sanity",
+    "name": "Entity Dissolving Entity",
+    "playcount": 23,
+    "playcountMinute": 85,
+    "playcountHour": "1.4"
+  }
+]
